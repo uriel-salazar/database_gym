3969428fff26f959ddb5ac99b289c66cb036638e
Urieel <urielsalazar.dev@gmail.com>
2026-03-08 13:10:09 -0600

    A a tiny test to try if declarative base and tables work  succesfully.

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine,String,Float,Column,Integer
-from sqlalchemy.orm import sessionmaker,relationship,Session,Mapped,mapped_column,declarative_base
+from sqlalchemy.orm import sessionmaker,relationship,Session,Mapped,mapped_column,declarative_base,DeclarativeBase
 from dotenv import load_dotenv
 import os 
 
@@ -7,5 +7,18 @@ load_dotenv()
 
 URL=os.getenv("DATABASE_URL")
 
-
 engine = create_engine(URL, echo=True)
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Client(Base):
+    __tablename__='client'
+    name:Mapped[str]=mapped_column(primary_key=True,nullable=False)
+    
+
+
+Base.metadata.create_all(engine) 
+Session=sessionmaker(bind=engine)
+session_local=Session()
