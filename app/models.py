330f83e5fef1a05b430ec5e758ba1eadfd75e091
Urieel <urielsalazar.dev@gmail.com>
2026-03-09 18:30:27 -0600

    Write logic for a relationship between use and membership

--- app/models.py
+++ app/models.py
@@ -1,23 +1,32 @@
-from sqlalchemy import Column, Integer, String,Float
-from sqlalchemy.orm import mapped_column,Mapped,sessionmaker
+from sqlalchemy import Column, Integer, String,Float,ForeignKey,Date
+from sqlalchemy.orm import mapped_column,Mapped,sessionmaker,relationship
 from typing_extensions import Annotated
 from database import Base,engine
+from datetime import datetime
 
-template_name=Annotated[int,mapped_column(String(60),primary_key=True)]
+template_name=Annotated[int,mapped_column(String(60),nullable=True)]
 
-class Client(Base):
+class User(Base):
     """_summary_
 
     Args:
         Base (_type_): This class inherits from the Declarative Base.
     """
-    __tablename__='client'
-    id:Mapped[int]= mapped_column(primary_key=True,nullable=False)
-    username:Mapped[str] = mapped_column(nullable=True)
+    __tablename__='user'
+    user_id:Mapped[list['Membership']]=relationship('membership')
+    name:Mapped[template_name]
     age:Mapped[int]= mapped_column(nullable=True)
+    status:Mapped[bool]= mapped_column(nullable=True)
 
 
-
+class Membership(Base):
+    __tablename__='membership'
+    id:Mapped[int]=mapped_column(primary_key=True)
+    user_id:Mapped[int]=mapped_column(ForeignKey('user.user_id'))
+    name:Mapped[template_name]
+    start_date:Mapped[Date]=mapped_column(nullable=True)
+    
+    
 Base.metadata.create_all(engine)
 Session=sessionmaker(bind=engine)
 session_local=Session()
