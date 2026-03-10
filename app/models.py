7ca161c21423bfa70878788588e40db116de0d79
Urieel <urielsalazar.dev@gmail.com>
2026-03-10 12:42:29 -0600

    Explaining the relationship between the tables

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String,Float,ForeignKey,Date
+from sqlalchemy import Integer, String,ForeignKey,DateTime
 from sqlalchemy.orm import mapped_column,Mapped,sessionmaker,relationship
 from typing_extensions import Annotated
 from database import Base,engine
-from datetime import datetime
+from datetime import date,datetime
 
 template_name=Annotated[int,mapped_column(String(60),nullable=True)]
 
@@ -13,18 +13,27 @@ class User(Base):
         Base (_type_): This class inherits from the Declarative Base.
     """
     __tablename__='user'
-    user_id:Mapped[list['Membership']]=relationship('membership')
+    
+    user_id:Mapped[int]=mapped_column(primary_key=True) 
     name:Mapped[template_name]
     age:Mapped[int]= mapped_column(nullable=True)
     status:Mapped[bool]= mapped_column(nullable=True)
-
-
+    
+    memberships:Mapped[list['Membership']]=relationship('Membership',back_populates='user')
+    """An user can have many memberships; therefore it's a list, it's a relationship between 
+    the membership table and this table."""
+    
 class Membership(Base):
     __tablename__='membership'
     id:Mapped[int]=mapped_column(primary_key=True)
-    user_id:Mapped[int]=mapped_column(ForeignKey('user.user_id'))
+    
+    # we will use user_id  as the foreign key from the user's table.
+
     name:Mapped[template_name]
-    start_date:Mapped[Date]=mapped_column(nullable=True)
+    start_date:Mapped[datetime]=mapped_column(DateTime,nullable=True)
+    
+    user_id:Mapped[int]=mapped_column(ForeignKey('user.user_id'))
+    users:Mapped['User']=relationship('user', back_populates='memberships')
     
     
 Base.metadata.create_all(engine)
