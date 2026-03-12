5c09db6bb59313b046d861065101edb222b87da3
Urieel <urielsalazar.dev@gmail.com>
2026-03-12 16:52:38 -0600

    Try a default endpoint with FASTAPI

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from models import User
-from schemas import Create_user
+from schemas import Create_user,Response_user
 
 def get_user(db:Session,user_id=User.user_id):
     return db.query(User).filter(User.user_id == user_id)
@@ -9,6 +9,13 @@ def get_user(db:Session,user_id=User.user_id):
 def create_user(db: Session,Create_user):
     db_user = User(name=Create_user.name, age=Create_user.age, status=Create_user.status)
     db.add(db_user) # adds the user to the table. 
-    db.commit() # commits the database operations 
+    db.commit() # commits the database operation
     db.refresh(db_user)
     return db_user # returns the created user. 
+
+def delete_user(db:Session,user_id=User.user_id):
+    user=db.query(Response_user).filter(User.user_id==user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
-
+import uvicorn
 
 app=FastAPI()
+ 
+@app.get("/")
+async def welcome():
+    return {"message":"Hello Word"}
 
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", 
+            host="localhost", reload=True)
