9e836b87e01e09184934f57de2c6efc475ddf344
Urieel <urielsalazar.dev@gmail.com>
2026-03-16 09:28:42 -0600

    Create  an  update endpoint and start to create vadidation for age
     Reject ' post' if  the user has 11 year old or less

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import uvicorn
 from sqlalchemy.orm import Session
 from fastapi import HTTPException,Depends
 from models import get_db,User
-from crud import create_user,get_users
+import crud
+#from crud import create_user,get_users,update_user
 from schemas import User_Create,User_Response
 from database import Base,engine
-
 
 app=FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -22,23 +22,36 @@ async def welcome():
     </html>"""
 
 @app.get("/users",response_model = list[User_Response]) 
-async def read_users(skip: int= 0, limit :int = 10 , db:Session = Depends(get_db)):
-    return get_users(db ,skip=skip,limit=limit)
+async def read_users(skip: int= 0, limit :int = 10,
+    db:Session = Depends(get_db)):
+    return crud.get_users(db ,skip=skip,limit=limit)
     
 
 
 @app.get("/user/{user_id}",response_model=User_Response)
-async def read_user(user_id:int,db:Session=Depends(get_db)):
+async def read_user(user_id:int,db: Session = Depends(get_db)):
       user=db.query(User).filter(User.user_id==user_id).first()
       if not user:
-          HTTPException(status_code=404,detail="User not found :( ")
+          raise HTTPException(status_code=404,detail="User not found :( ")
       return user
   
 
 @app.post("/users",response_model=User_Response)
 async def create(user_create:User_Create,db:Session=Depends(get_db)):
-    return create_user(db,user_create)
+    
+    if user_create.age  <=  11:
+        raise HTTPException(status_code=403,detail="You're too young")
+    return crud.create_user(db,user_create)
 
+
+@app.put("/users/{user_id}",response_model=User_Response)
+async def update_user(user_id:int, user_update:User_Create,db: Session = Depends(get_db)):    
+    update= crud.update_user(db,user_id,user_update)
+    if update is None:
+        raise HTTPException(status_code=404,detail='User not found')
+    
+    return update
+    
 if __name__ == "__main__":
     uvicorn.run("main:app", 
             host="localhost", reload=True)
